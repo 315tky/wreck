184d8a788b54a95fa769588c02c05fbd90bd6138
315tky <315tky@gmail.com>
2020-03-05 07:38:28 +0900

    initial landing page

--- dev/app/rails/app/models/killmail.rb
+++ dev/app/rails/app/models/killmail.rb
@@ -1,25 +1,34 @@
 class Killmail < ApplicationRecord
+
+  scope :kills, -> { where.not(victim_corporation_id: 98473505) }
+  scope :losses, -> { where(victim_corporation_id: 98473505) }
+
   has_many :killmail_attackers, :primary_key => "killmail_id", :foreign_key => "killmail_id", dependent: :destroy
   has_many :killmail_items, :primary_key => "killmail_id", :foreign_key =>  "killmail_id", dependent: :destroy
   belongs_to :solarsystem, :primary_key => "solar_system_id", :foreign_key => "solar_system_id", :optional => true
 
-  def home_kills(home_corp_id)
-    home_kills = []
-    self.each do |killmail|
-      if killmail.victim_corporation_id != corp_id
-        home_kills.push(killmail)
-      end
+
+  def self.get_by_time(time_frame)
+    if time_frame == "all"
+      kills_list = self.kills
+      top_ten_attackers(kills_list)
+    elsif time_frame == "month"
+      kills_list = self.kills.where(killmail_time: 1.month.ago..)
+      top_ten_attackers(kills_list)
+    elsif time_frame == "week"
+      kills_list = self.kills.where(killmail_time: 1.week.ago..)
+      top_ten_attackers(kills_list)
     end
-    return home_kills
   end
 
-  def home_losses(home_corp_id)
-    home_losses = []
-    self.each do |killmail|
-      if killmail.victim_corporation_id == corp_id
-        home_losses.push(killmail)
-      end
+  def self.top_ten_attackers(kills_list)
+    hash = {}
+    all_attackers = kills_list.map { |kill| kill.killmail_attackers }
+    attackers = all_attackers.map { |attackers| attackers.map { |attacker| attacker.attacker_id } }
+    attackers.flatten.compact.tally.sort_by { |k,v| -v }.first(10).each do |attacker|
+      hash[Character.where(character_id: attacker[0]).first.name] = attacker[1]
     end
-    return home_losses
+    pp hash
+    return hash
   end
 end
