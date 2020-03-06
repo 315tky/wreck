157599a3329e5cae829b969f769fbed87742fb9a
315tky <315tky@gmail.com>
2020-03-06 16:01:34 +0900

    tidied dev config

--- dev/tasks/eve_mysql_data_import/mysql_sde_import.sh
+++
@@ -1,5 +0,0 @@
-############### THIS IS OLD UNUSED, REMOVE AT SOME POINT
-#!/bin/bash
-#mysql -u username -p dbname <  <sde sql file>
-#import to mysql using mysql container
-docker exec -i pokill_dev_container_mysql_db_1 sh -c 'exec mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DATABASE' < sde-*/sde*
