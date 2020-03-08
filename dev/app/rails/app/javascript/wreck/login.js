7864680264ac90541f6f331d45b6ddc708af0b1e
315tky <315tky@gmail.com>
2020-03-08 23:08:55 +0900

    login js

--- dev/app/rails/app/javascript/wreck/login.js
+++ dev/app/rails/app/javascript/wreck/login.js
@@ -5,19 +5,19 @@ document.addEventListener("DOMContentLoaded", function(){
   login.addEventListener("click", myFunction);
 
   function myFunction() {
-
+    location.href = "https://login.eveonline.com/v2/oauth/authorize?response_type=code&redirect_uri=http://localhost:8080/user/login&client_id=0ad0f6f59d594782b795023d1d6ec5d3&scope=esi-characters.read_standings.v1&state=thisistheuniquestring";
     console.log("hello world")
-    fetch('https://pokill.saigos.space', {
-      mode: 'no-cors' // suppress 'cors' warning for now
-    })
-    .then((response) => {
-      return response;
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch(function(error) {
-      console.log('Looks like there was a problem: \n', error);
-    });
+    //fetch('https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://pokill.saigos.space&client_id=0ad0f6f59d594782b795023d1d6ec5d3&scope=esi-characters.read_standings.v1', {
+    //  mode: 'no-cors' // suppress 'cors' warning for now
+    //})
+    //.then((response) => {
+    //  return response;
+    //})
+    //.then((data) => {
+    //  console.log(data);
+    //})
+    //.catch(function(error) {
+    //  console.log('Looks like there was a problem: \n', error);
+    //});
   };
 });
