bb2e86f8ac47980c2645d3cb3cbf8d9d9da2c5e5
315tky <315tky@gmail.com>
2020-03-31 13:42:07 +0900

    kills and losses by char

--- dev/app/rails/app/controllers/killmails_controller.rb
+++ dev/app/rails/app/controllers/killmails_controller.rb
@@ -1,2 +1,11 @@
 class KillmailsController < ApplicationController
+
+  def kills_by_char
+    @pagy_kills, @kills_by_char = pagy(KillmailsWithFinalBlowAttacker.kills_by_char(params[:char_name]))
+  end
+
+  def losses_by_char
+    @pagy_losses, @losses_by_char = pagy(KillmailsWithFinalBlowAttacker.losses_by_char(params[:char_name]))
+  end
+
 end
--- dev/app/rails/config/routes.rb
+++ dev/app/rails/config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   get '/users/:id', to: 'users#show', as: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'killmails/kills_by_char/:char_name', to: 'killmails#kills_by_char', as: 'kills'
+  get 'killmails/losses_by_char/:char_name', to: 'killmails#losses_by_char', as: 'losses'
+
 end
