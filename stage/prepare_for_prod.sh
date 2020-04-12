4fcf38bb4cc08ffb49895182a81441bde7ec16da
315tky <315tky@gmail.com>
2020-04-12 17:05:35 +0900

    minor update to prod prepare script

--- stage/prepare_for_prod.sh
+++ stage/prepare_for_prod.sh
@@ -13,6 +13,7 @@ docker push 315tky/wreck:nginx_proxy_latest
 docker push 315tky/wreck:postgres_latest
 docker push 315tky/wreck:mysql_latest
 
+echo "do scp as \"nath\" account"
 echo "copy the precompiled assets to /app/wreck/public/assets on Prod host"
 echo "From Prod host, docker pull the 315/wreck images"
 echo "Copy relevant files over, like updated docker-compose, tasks dir etc.."
